140625908b4c81c828e64e7fc25e3a63925e258b
tanerjn <stanermetin@gmail.com>
2023-09-11 15:02:36 +0200

    add keys

--- c1-versions.tf
+++ c1-versions.tf
@@ -13,7 +13,13 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   profile = "default"
+  access_key = "AKIA3TAMK6ZMEV4BJ3PX"
+  aws_secret_access_key = "olddkq/p3Qa8S03SOsQw/FxbpluZ28g0KBYVWJ0F"
 }
+
+
+
+
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
