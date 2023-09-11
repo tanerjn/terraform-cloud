031f7b6b95c295c9d63ccfe6e29850aa79134f4b
tanerjn <stanermetin@gmail.com>
2023-09-11 15:14:17 +0200

    refer to credentials file

--- c1-versions.tf
+++ c1-versions.tf
@@ -12,9 +12,8 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default"
-  access_key = "AKIA3TAMK6ZMEV4BJ3PX"
-  secret_key = "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"
+  profile = "terraform_user"
+  shared_credentials_file = "/.aws/credentials"
 }
 
 
